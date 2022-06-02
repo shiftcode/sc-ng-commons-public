c4cf7c85502767d1a96423bf3ad54a20b7243520
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2022-06-02 22:13:12 +0200

    refactor(auto-focus.directive): cleanup

--- libs/components/src/lib/auto-focus/auto-focus.directive.ts
+++ libs/components/src/lib/auto-focus/auto-focus.directive.ts
@@ -1,33 +1,24 @@
 import { DOCUMENT } from '@angular/common'
-import { Directive, ElementRef, Inject, OnInit } from '@angular/core'
+import { Directive, ElementRef, inject, OnInit } from '@angular/core'
 import { Logger, LoggerService } from '@shiftcode/ngx-core'
 
 @Directive({ selector: '[scAutoFocus]' })
 export class AutoFocusDirective implements OnInit {
-  private readonly logger: Logger
-  private document: Document
-
-  constructor(loggerService: LoggerService, private elRef: ElementRef, @Inject(DOCUMENT) doc: any) {
-    this.logger = loggerService.getInstance('AutoFocusDirective')
-    this.document = doc
-  }
-
-  get nativeElement() {
-    return this.elRef.nativeElement
-  }
+  readonly element: HTMLElement = inject(ElementRef).nativeElement
+  private readonly document: Document = inject(DOCUMENT)
+  private readonly logger: Logger = inject(LoggerService).getInstance('AutoFocusDirective')
 
   ngOnInit(): void {
-    const currentTabindex = this.nativeElement.getAttribute('tabindex')
-    // tslint:disable-next-line:triple-equals
-    if (!currentTabindex || parseInt(currentTabindex, 10) != currentTabindex) {
-      this.nativeElement.setAttribute('tabindex', '-1')
+    const currentTabindex = this.element.getAttribute('tabindex')
+    if (!currentTabindex || String(parseInt(currentTabindex, 10)) !== currentTabindex) {
+      this.element.setAttribute('tabindex', '-1')
     }
     this.focus()
   }
 
   focus() {
-    this.logger.debug('try get focus on', this.nativeElement)
-    this.nativeElement.focus()
+    this.logger.debug('try get focus on', this.element)
+    this.element.focus()
     this.logger.debug('active element:', this.document.activeElement)
   }
 }
