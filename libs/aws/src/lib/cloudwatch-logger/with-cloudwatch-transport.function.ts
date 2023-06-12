d819b81f4f1e01336d9ac1f893962158ecbc984d
Michael Wittwer <michael.wittwer@github.com>
2023-06-12 14:11:31 +0200

    feat(aws): provide possibilty to provide plain provider
    BREAKING CHANGE:
    Providing a function which is then used as a FactoryProvider is no longer possible. Either provide the CloudWatchLogTransportConfig or a Provider

--- libs/aws/src/lib/cloudwatch-logger/with-cloudwatch-transport.function.ts
+++ libs/aws/src/lib/cloudwatch-logger/with-cloudwatch-transport.function.ts
@@ -2,14 +2,23 @@ import { LOG_TRANSPORTS, LoggerFeature, LoggerFeatureKind } from '@shiftcode/ngx
 import { CLOUD_WATCH_LOG_TRANSPORT_CONFIG } from './cloud-watch-log-transport-config.injection-token'
 import { CloudWatchLogTransportConfig } from './cloud-watch-log-transport-config.model'
 import { CloudWatchLogTransport } from './cloud-watch-log-transport.service'
+import { Provider } from '@angular/core'
+
+function isCloudWatchTransportConfig(v: CloudWatchLogTransportConfig | Provider): v is CloudWatchLogTransportConfig {
+  return (<Array<keyof CloudWatchLogTransportConfig>>[
+    'logLevel',
+    'logGroupName',
+    'clientConfig$',
+    'flushInterval',
+  ]).every((propertyName) => propertyName in v)
+}
 
 export function withCloudwatchTransport(
-  cloudWatchLogTransportConfigOrFactory: CloudWatchLogTransportConfig | (() => CloudWatchLogTransportConfig),
+  cloudWatchLogTransportConfigOrProvider: CloudWatchLogTransportConfig | Provider,
 ): LoggerFeature {
-  const configProvider =
-    typeof cloudWatchLogTransportConfigOrFactory === 'function'
-      ? { provide: CLOUD_WATCH_LOG_TRANSPORT_CONFIG, useFactory: cloudWatchLogTransportConfigOrFactory }
-      : { provide: CLOUD_WATCH_LOG_TRANSPORT_CONFIG, useValue: cloudWatchLogTransportConfigOrFactory }
+  const configProvider: Provider = isCloudWatchTransportConfig(cloudWatchLogTransportConfigOrProvider)
+    ? { provide: CLOUD_WATCH_LOG_TRANSPORT_CONFIG, useValue: cloudWatchLogTransportConfigOrProvider }
+    : cloudWatchLogTransportConfigOrProvider
 
   return {
     kind: LoggerFeatureKind.TRANSPORT,
