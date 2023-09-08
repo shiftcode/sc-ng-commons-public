69b0c83daeaaf18ddade008da7f624b790df07fb
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2023-09-08 11:35:48 +0200

    feat(insert-view-ref.directive): add public `insert` and `detach` methods + `hasAttachedView` getter

--- libs/components/src/lib/insert-view-ref/insert-view-ref.directive.ts
+++ libs/components/src/lib/insert-view-ref/insert-view-ref.directive.ts
@@ -15,16 +15,42 @@ import { Directive, Input, ViewContainerRef, ViewRef } from '@angular/core'
  */
 @Directive({ selector: '[scInsertViewRef]', standalone: true })
 export class InsertViewRefDirective {
-  private _insertedViewRef: ViewRef | null = null
-
   @Input()
   set scInsertViewRef(val: ViewRef | null | undefined) {
-    if (this._insertedViewRef) {
-      this.container.remove(this.container.indexOf(this._insertedViewRef))
+    this.container.clear()
+    if (val) {
+      this.insert(val)
     }
+  }
 
-    this._insertedViewRef = val ? this.container.insert(val) : null
+  get hasAttachedView(): boolean {
+    return this.container.length > 0
   }
 
   constructor(private container: ViewContainerRef) {}
+
+  /**
+   * Inserts a view into the container.
+   * @throws when a view is already attached
+   */
+  insert(val: ViewRef) {
+    if (this.hasAttachedView) {
+      throw new Error('container already has a view attached')
+    }
+    this.container.insert(val)
+  }
+
+  /**
+   * Detaches the view from the container and returns its {@link ViewRef}.
+   * When detaching a view, it will no longer be auto-destroyed when the directive gets destroyed
+   * therefore it's necessary to manually destroy it manually at some point
+   * @throws when no view attached
+   */
+  detach(): ViewRef {
+    const r = this.container.detach(0)
+    if (!r) {
+      throw new Error('container has no view attached')
+    }
+    return r
+  }
 }
