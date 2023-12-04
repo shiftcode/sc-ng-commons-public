40cd7153dbaecc8faf68d973980fb6c80c89a9bf
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2023-12-04 16:08:32 +0100

    feat(resolve-fn-to-can-activate-fn): new utility to transform a ResolveFn to a CanActivateFn

---
+++ libs/core/src/lib/static-utils/resolve-fn-to-can-activate-fn.function.spec.ts
@@ -0,0 +1,12 @@
+import { ResolveFn } from '@angular/router'
+import { expect } from '@jest/globals'
+import { resolveFnToCanActivateFn } from './resolve-fn-to-can-activate-fn.function'
+
+describe('resolveFnToCanActivateFn', () => {
+  const resolver1: ResolveFn<any> = () => Promise.resolve({ foo: 'bar' })
+
+  test('returns a function', () => {
+    const guard = resolveFnToCanActivateFn(resolver1)
+    expect(guard).toBeInstanceOf(Function)
+  })
+})
---
+++ libs/core/src/lib/static-utils/resolve-fn-to-can-activate-fn.function.ts
@@ -0,0 +1,19 @@
+import { CanActivateFn, createUrlTreeFromSnapshot, ResolveFn } from '@angular/router'
+import { toPromise } from './to-promise.function'
+
+/**
+ * takes an angular {@link ResolveFn} and returns a {@link CanActivateFn}
+ * by simply wrapping the ResolveFn and applying a truthy check on its result (with `!!result`)
+ * @returns true or a {@link RouterTree} which is relative to the current routeSnapshot.
+ * @example
+ * ```ts
+ *  const resolveFn: ResolveFn<MyDto|null> = (route: ActivatedRouteSnapshot) => inject(MyApi).fetch(route.params.id)
+ *  const canActivateFn: CanActivateFn = resolveFnToCanActivateFn(resolveFn, ['..', 'relative-other-route'])
+ *  ```
+ */
+export function resolveFnToCanActivateFn<T extends object>(
+  resolveFn: ResolveFn<T | null | undefined>,
+  relativeRouteRedirect: any[] = ['..'],
+): CanActivateFn {
+  return (r, s) => toPromise(resolveFn(r, s)).then((v) => !!v || createUrlTreeFromSnapshot(r, relativeRouteRedirect))
+}
