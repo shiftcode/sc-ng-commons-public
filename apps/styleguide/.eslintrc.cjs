58e286a7f3c056ccbd53b29c2c8b6ea47a407040
Michael Wittwer <michael.wittwer@shiftcode.ch>
2025-01-20 09:56:36 +0100

    refactor(styleguide): cleanup .eslintrc.cjs

--- apps/styleguide/.eslintrc.cjs
+++ apps/styleguide/.eslintrc.cjs
@@ -7,59 +7,15 @@ module.exports = {
       plugins: ['@shiftcode/rules'],
       files: ['*.ts'],
       rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
-          {packageDir: '../..'},
-
-        ],
+        'import/no-extraneous-dependencies': ['error', { packageDir: '../..' }],
         'import/no-internal-modules': [
           'error',
           {
-            allow: [
-              'rxjs/*',
-            ],
+            allow: ['rxjs/*'],
           },
         ],
-
-  //       '@typescript-eslint/member-ordering': 'off',
-  //       /*
-  //        * from tslint
-  // no-submodule-imports:
-  //   - true
-  //   - "app"
-  //   - aws-sdk
-  //   - "@angular"
-  //   - "zone.js"
-  //   - "aws-sdk/clients"
-  //   - "date-fns/esm/locale"
-  //   - "@apollo/client"
-  //        */
-  //       '@shiftcode/rules/import-denylist': [
-  //         // TSLint: "import-blacklist"
-  //         'error',
-  //         {
-  //           patterns: [
-  //             /\.\/(core|shared)\/.*/, // use app/* instead
-  //           ],
-  //         },
-  //       ],
-  //       '@typescript-eslint/naming-convention': [
-  //         'error',
-  //         {
-  //           selector: 'property',
-  //           modifiers: ['private', 'static'],
-  //           format: ['camelCase', 'UPPER_CASE', 'snake_case'],
-  //         },
-  //       ],
       },
     },
-    // {
-    //   files: ['*.html'],
-    //   rules: {
-    //     // could potentially make sense to activate that rule, but we never had it also with tslint
-    //     '@angular-eslint/template/no-negated-async': 'off',
-    //   },
-    // },
   ],
   parserOptions: {
     project: ['./tsconfig.eslint.json'],
