fd63419750d8f045fa969a439d0e9fa7640e3973
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-01-23 11:49:33 +0100

    test(logging-time-format): let it succeed locally & ci

--- libs/core/src/lib/logger/helper/logging-time-format.const.spec.ts
+++ libs/core/src/lib/logger/helper/logging-time-format.const.spec.ts
@@ -2,17 +2,17 @@ import { loggingTimeFormat } from './logging-time-format.const'
 
 describe('timeFormat', () => {
   it('should format time as HH:mm:ss.SSS', () => {
-    const timestamp = new Date('2024-01-15T14:30:45.123Z')
+    const timestamp = new Date('2024-01-15T14:30:45.123')
     expect(loggingTimeFormat.format(timestamp)).toBe(`14:30:45.123`)
   })
 
   it('should format another time as HH:mm:ss.SSS', () => {
-    const timestamp = new Date('2024-01-01T23:59:59.999Z')
+    const timestamp = new Date('2024-01-01T23:59:59.999')
     expect(loggingTimeFormat.format(timestamp)).toBe(`23:59:59.999`)
   })
 
   it('should format time with leading zeros as HH:mm:ss.SSS', () => {
-    const timestamp = new Date('2024-01-01T01:02:03.004Z')
+    const timestamp = new Date('2024-01-01T01:02:03.004')
     expect(loggingTimeFormat.format(timestamp)).toBe(`01:02:03.004`)
   })
 })
