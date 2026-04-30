81dc82b757caf89cc55d2d9b4c4096eea03f9b9b
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-04-30 08:45:33 +0200

    feat: new utility `onInit` function to replace ngOnInit

---
+++ libs/core/src/lib/static-utils/on-init.function.ts
@@ -0,0 +1,25 @@
+import { effect, EffectCleanupFn, untracked } from '@angular/core'
+
+/**
+ * Utility function to replace ngOnInit method.
+ * Runs as an effect exactly once and uses the returned function as the cleanup, which will be executuded onDestroy
+ * Needs to be run in an injection context (e.g. in your constructor).
+ *
+ * @example
+ * ```ts
+ *   onInit(() => {
+ *     const subscription = someObservable.subscribe(...)
+ *     return () => subscription.unsubscribe()
+ *   })
+ * ```
+ */
+export function onInit(initFn: () => void | EffectCleanupFn) {
+  // we use `untracked` to ensure the effect is called only once as no dependency will re-trigger it.
+  // the cleanup is then effectively called in the the ngOnDestroy lifecycle.
+  effect((onCleanup) => {
+    const cleanup = untracked(initFn)
+    if (typeof cleanup === 'function') {
+      onCleanup(cleanup)
+    }
+  })
+}
