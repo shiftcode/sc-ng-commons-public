c661200017385859753afa9a9e815124b206997e
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2026-04-30 08:47:37 +0200

    feat(logger): enhance `withCustomLogTransport` to support `useExisting` option

--- libs/core/src/lib/logger/with-custom-log-transport.function.ts
+++ libs/core/src/lib/logger/with-custom-log-transport.function.ts
@@ -7,10 +7,17 @@ import { LoggerFeatureKind } from './logger-feature-kind.enum'
 /**
  * LoggerFeature to use with {@link provideLogger} that registers a custom LogTransport implementation.
  * @param transportClass - The LogTransport implementation class to use
+ * @param useExisting - If true, the transportClass will be registered with `useExisting` instead of `useClass`.
  */
-export function withCustomLogTransport(transportClass: Type<LogTransport>): LoggerFeature {
+export function withCustomLogTransport(transportClass: Type<LogTransport>, useExisting = false): LoggerFeature {
   return {
     kind: LoggerFeatureKind.TRANSPORT,
-    providers: [{ provide: LogTransport, useClass: transportClass, multi: true }],
+    providers: [
+      {
+        provide: LogTransport,
+        multi: true,
+        ...(useExisting ? { useExisting: transportClass } : { useClass: transportClass }),
+      },
+    ],
   }
 }
